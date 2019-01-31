a3b9a6ca7e6d2adc4ae5d7296b3812e74adc913a
Oscar canton <oscar@imac-de-oscar.local>
2019-01-31 21:31:49 +0100

    Initial Commit

---
+++ Westeros/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Westeros
+//
+//  Created by Oscar canton on 31/1/19.
+//  Copyright © 2019 Oscar canton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
